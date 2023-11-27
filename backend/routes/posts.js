7077db94dc774d9a5173ed3e17b312520cc74e6c
Dev Yadav <devyadav0399@gmail.com>
2023-11-28 00:56:41 +0530

    Update post routes to handle 404 errors

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
-// Require the post model
+
 const Post = require("../models/post");
 
 /* GET posts */
 router.get("/", async (req, res, next) => {
   // sort from the latest to the earliest
   const posts = await Post.find().sort({ createdAt: "desc" });
+  if (!posts) {
+    return res.status(404).json({
+      statusCode: 404,
+      message: "No posts found",
+    });
+  }
   return res.status(200).json({
     statusCode: 200,
     message: "Fetched all posts",
@@ -18,11 +24,17 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   // req.params contains the route parameters and the id is one of them
   const post = await Post.findById(req.params.id);
+  if (!post) {
+    return res.status(404).json({
+      statusCode: 404,
+      message: "Post not found",
+    });
+  }
   return res.status(200).json({
     statusCode: 200,
     message: "Fetched post",
     data: {
-      post: post || {},
+      post: post,
     },
   });
 });
@@ -52,8 +64,17 @@ router.post("/", async (req, res, next) => {
 router.put("/:id", async (req, res, next) => {
   const { title, author, content, tags } = req.body;
 
+  const post = await Post.findById(req.params.id);
+
+  if (!post) {
+    return res.status(404).json({
+      statusCode: 404,
+      message: "Post not found",
+    });
+  }
+
   // findByIdAndUpdate accepts the post id as the first parameter and the new values as the second parameter
-  const post = await Post.findByIdAndUpdate(req.params.id, {
+  const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
     title,
     author,
     content,
@@ -63,7 +84,7 @@ router.put("/:id", async (req, res, next) => {
   return res.status(200).json({
     statusCode: 200,
     message: "Updated post",
-    data: { post },
+    data: { updatedPost },
   });
 });
 
@@ -71,6 +92,12 @@ router.put("/:id", async (req, res, next) => {
 router.delete("/:id", async (req, res, next) => {
   // Mongo stores the id as `_id` by default
   const result = await Post.deleteOne({ _id: req.params.id });
+  if (result.deletedCount === 0) {
+    return res.status(404).json({
+      statusCode: 404,
+      message: "Post not found",
+    });
+  }
   return res.status(200).json({
     statusCode: 200,
     message: `Deleted ${result.deletedCount} post(s)`,
