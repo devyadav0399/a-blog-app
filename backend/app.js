4815c73c3d710ce413ebe0ef75f5c59695ec6ed8
Dev Yadav <devyadav0399@gmail.com>
2023-11-27 19:54:36 +0530

    Add posts route & post model

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ db.once('open', () => {
 db.on('error', console.error.bind(console, 'connection error:'));
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
 
 const app = express();
 
@@ -37,7 +37,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/api/posts', postsRouter)
+
+// Return the client
+app.get('/posts*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'public') + '/index.html')
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ backend/models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: String, required: true },
+  tags: { type: [String] },
+  createdAt: { type: Date, default: Date.now },
+});
+
+PostSchema.pre("save", (next) => {
+  now = new Date();
+  if (!this.createdAt) {
+    this.createdAt = now;
+  }
+  next();
+});
+
+module.exports = mongoose.model("Post", PostSchema);
